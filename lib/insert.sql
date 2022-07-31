1ff37b72bbded4ac1ecd030762a83bbcb7115eaf
noelensith <mbnoelen@gmail.com>
2022-07-31 16:29:56 -0700

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Happy', 0);
+INSERT INTO bears VALUES (2, 'Rowdy', 12, 'M', 'White', 'Mean', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 21, 'F', 'Grey', 'Nice', 1);
+INSERT INTO bears VALUES (4, 'Sargeant Brown', 25, 'M', 'Brown', 'Bold', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 1, 'F', 'Black', 'Sweet', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 14, 'M', 'Green', 'Grumpy', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 10, 'F', 'Red', 'Jubilant', 0);
+INSERT INTO bears VALUES (8, null, 13, 'F', 'Blue', 'Sad', 1);
+
